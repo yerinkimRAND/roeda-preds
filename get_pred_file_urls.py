096a7b92464493e30c74f65f8ac6525c0e83aed1
yerinkim <yerinkim@rand.org>
2019-11-07 15:10:06 -0500

    added url script

---
+++ get_pred_file_urls.py
@@ -0,0 +1,19 @@
+import os
+import csv
+
+##commit pred files to git first
+
+#get relevant file names
+in_dir="preds"
+with os.scandir(in_dir) as dir:
+	file_list = [file.name for file in dir if file.is_file() == True]
+
+#write url file
+with open('pred_urls.csv', mode='w', newline='') as url_file:
+    csv_writer = csv.writer(url_file, delimiter=',')
+
+    #write header
+    csv_writer.writerow(['pred_file', 'url'])
+
+    for file in file_list:
+    	csv_writer.writerow([file, "https://code.rand.org/yerinkim/roeda-preds/raw/master/preds/{0}".format(file)])
