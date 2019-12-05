67de4c841421820aa7e746bcefa5eba03cd39fc3
yerinkim <yerinkim@rand.org>
2019-12-05 14:25:30 -0500

    reload preds for sma nd pitt

--- get_pred_file_urls_s3.py
+++ get_pred_file_urls_s3.py
@@ -4,16 +4,16 @@ import csv
 ##commit pred files to git first
 
 #get relevant file names
-in_dir="preds/sm"
+in_dir="preds"
 with os.scandir(in_dir) as dir:
 	file_list = [file.name for file in dir if file.is_file() == True]
 
 #write url file
-with open('pred_urls_sm.csv', mode='w', newline='') as url_file:
+with open('pred_urls_s3.csv', mode='w', newline='') as url_file:
     csv_writer = csv.writer(url_file, delimiter=',')
 
     #write header
     csv_writer.writerow(['pred_file', 'url'])
 
     for file in file_list:
-    	csv_writer.writerow([file, "https://raw.githubusercontent.com/yerinkimRAND/roeda-preds/master/preds/sm/{0}".format(file)])
+    	csv_writer.writerow([file, "s3://accs-data.rand.cloud/services/roeda/{0}".format(file)])
